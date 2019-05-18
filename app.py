d7b12e61cc60a3729334a6d8b56a066663bb0f0a
Peyman Shakari <peyman@shakari.no>
2019-05-18 21:31:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,46 @@
-from flask import Flask
+import os
+from os import environ
+import shutil
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import mean
+from plotly.offline import plot
+import argparse
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-      
-    spark_session = SparkSession.builder.appName('winemap2').getOrCreate()   
-    #df = spark_session.read.format("jdbc").options(url=url,dbtable="population",driver="org.postgresql.Driver").load()
-    #table = df.select('continent', 'sum(population) population').groupBy('continent').orderBy('population', ascending=False)
-    continents = []#table.select('continent').collect()
-    populationSum = []#table.select('population').collect()		
-		
-    return 'hello'
+
+class WineMapGenerator:
+
+    def __init__(self):
+        server = environ.get("SERVER")
+        user = environ.get("USER")
+        password = environ.get("PASSWORD")
+        dbname = environ.get("DBNAME")
+        self.make(server,
+                  user, dbname, password)
+
+    def make(self, server, user, dbname, password):
+        spark_session = SparkSession.builder.appName('winemap').getOrCreate()
+      	
+
+
+def make_template():
+    # make the templates dir
+    new_path = '/opt/app-root/src/templates'
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+        # move the file to the templates dir
+        shutil.move('/opt/app-root/src/map.html', new_path)
+    return render_template("map.html", title='Maps')
+
+@app.route('/')
+def index():
+    WineMapGenerator()
+    return 'halllooo'
+
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
